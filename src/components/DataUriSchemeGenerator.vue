c46bfa4ed248bbbc5d2d8de76dbf859735520d1d
Tomohito Higuchi <tomgoodsun@gmail.com>
2021-11-09 23:37:17 +0900

    [modify] Use v-for for result

--- src/components/DataUriSchemeGenerator.vue
+++ src/components/DataUriSchemeGenerator.vue
@@ -9,7 +9,38 @@
           v-on:change="readFromFile"
         ></b-form-file>
         <div class="mt-3 images">
-          <div class="result"></div>
+          <div class="result">
+
+            <div v-for="(value) in result" v-bind:key="value.name" class="img-line">
+              <div class="img-name">{{ value.file.name }}</div>
+              <div class="img">
+                <img
+                  :src="value.event.srcElement.result"
+                  :name="value.file.name"
+                  :alt="value.file.name"
+                />
+              </div>
+              <div class="data-uri-scheme">
+                <textarea
+                  :id="value.id"
+                  v-bind:value="value.event.srcElement.result"
+                >
+                </textarea>
+              </div>
+              <div class="op-btn">
+                <b-button
+                  variant="light"
+                  size="sm"
+                  class="clipboard"
+                  :data-clipboard-target="value.target"
+                  title="Copy to clipboard"
+                >
+                  <b-icon icon="clipboard" aria-hidden="true"></b-icon> Copy
+                </b-button>
+              </div>
+            </div>
+
+          </div>
         </div>
       </b-col>
     </b-row>
@@ -44,35 +75,14 @@ Vue.use(FormInputPlugin);
 Vue.use(FormPlugin);
 
 let resultAreas = [];
-
-async function loadImage(index, file) {
-  let fr = new FileReader();
-  fr.onload = function (evt) {
-    let html = '';
-    html += `<div class="img-line">`;
-    html += `  <div class="img-name">${file.name}</div>`;
-    html += `  <div class="img">`;
-    html += `    <img src="${fr.result}" name="${file.name}" alt="${file.name}" />`;
-    html += `  </div>`;
-    html += `  <div class="data-uri-scheme">`;
-    html += `    <textarea id="data-uri-scheme-result-${index}">${evt.target.result}</textarea>`;
-    html += `  </div>`;
-    html += `  <div class="op-btn">`;
-    html += `    <button variant="light" size="sm" class="btn btn-sm btn-light clipboard" data-clipboard-target="#data-uri-scheme-result-${index}" title="Copy to clipboard">`;
-    html += `      <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="clipboard" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-clipboard b-icon bi"><g><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"></path><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"></path></g></svg> Copy`;
-    html += `    </button>`;
-    html += `  </div>`;
-    html += `</div>`;
-    resultAreas.forEach(element => {
-      element.innerHTML += html;
-    });
-  };
-  await fr.readAsDataURL(file);
-}
+let fileCount = 0;
+let frResults = [];
+let frResultCount = 0;
 
 export default {
   data() {
     return {
+      result: []
     }
   },
   mounted() {
@@ -84,11 +94,43 @@ export default {
         element.innerHTML = '';
       });
       let files = evt.target.files;
-      for (let i = 0, len = files.length; i < len; i++) {
-        loadImage(i, files[i]);
+
+      fileCount = files.length;
+      frResults = [];
+      this.result = [];
+      frResultCount = 0;
+
+      for (let i = 0; i < fileCount; i++) {
+        this.loadImage(i, files[i]);
       };
-      new Clipboard('.clipboard');
+      this.checkImageLoading();
+    },
+
+    loadImage(index, file) {
+      let fr = new FileReader();
+      fr.onload = function (evt) {
+        frResults[index] = {
+          index: index,
+          id: 'data-uri-scheme-result-' + index,
+          target: '#data-uri-scheme-result-' + index,
+          file: file,
+          event: evt
+        };
+        frResultCount++;
+      };
+      fr.readAsDataURL(file);
+    },
+
+    checkImageLoading() {
+      console.log(frResults);
+      if (fileCount == frResultCount) {
+        this.result = frResults;
+        new Clipboard('.clipboard');
+        return;
+      }
+      setTimeout(this.checkImageLoading, 500);
     }
+
   },
   components: {
     BFormTextarea
