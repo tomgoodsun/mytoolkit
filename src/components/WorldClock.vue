60d8a95bb1cc92f2ce84ca755e5f7ff6f5b059d4
Tomohito Higuchi <tomgoodsun@gmail.com>
2022-07-27 02:07:07 +0900

    Add color definition for time

--- src/components/WorldClock.vue
+++ src/components/WorldClock.vue
@@ -2,7 +2,9 @@
   <b-row>
     <b-col col lg="12" md="12" sm="12">
       <div v-for="timezone in timezones" :key="timezone.utc" :id=timezone.utc class="timezone">
-        <div class="utc">UTC {{timezone.utc}}</div>
+        <div class="utc">
+          UTC {{timezone.utc}}
+        </div>
         <div class="clock">
           2022-01-01 00:00:00.123
         </div>
@@ -32,20 +34,71 @@ export default {
     };
   },
   mounted() {
+    let findColor = (targetTime) => {
+      let time = null;
+      let color = null;
+      //console.log(WorldClockTimeZones.COLOR_DEFINITIONS);
+      for (let i in WorldClockTimeZones.COLOR_DEFINITIONS) {
+        time = WorldClockTimeZones.COLOR_DEFINITIONS[i][0];
+        color = WorldClockTimeZones.COLOR_DEFINITIONS[i][1];
+        //console.log(`${time} > ${targetTime}`);
+        if (time >= targetTime) {
+          break;
+        }
+      }
+      //console.log(`${time} >= ${targetTime} | ${color}`);
+      return color;
+    };
+
     let counter = () => {
       for (let i in WorldClockTimeZones.LIST) {
         let utc = WorldClockTimeZones.LIST[i].utc;
-        let dateStr = DateTime
+        let date = DateTime
           .local()
-          .toUTC(WorldClockTimeZones.LIST[i].utc_min)
-          .toFormat('yyyy-LL-dd (ccc) HH:mm:ss.SSS');
-        document.getElementById(utc).querySelectorAll('.clock').forEach((element) => {
-          element.innerHTML = dateStr;
-        });
+          .toUTC(WorldClockTimeZones.LIST[i].offsetMin);
+
+        let targetTime = date.toFormat('HHmm');
+        let dateStr = date.toFormat('yyyy-LL-dd (ccc) HH:mm:ss.SSS');
+
+        let element = document.getElementById(utc);
+        element.style.backgroundColor = findColor(targetTime);
+        element
+          .querySelectorAll('.clock')
+          .forEach((element) => {
+            element.innerHTML = dateStr;
+          });
       }
       setTimeout(counter, 1);
     };
     counter();
+
+    let testCounter = () => {
+      let addMin = 0;
+      let date = DateTime.local(2022, 1, 1, 0, 0, 0, 0);
+      let counter = () => {
+        for (let i in WorldClockTimeZones.LIST) {
+          let utc = WorldClockTimeZones.LIST[i].utc;
+          let tmpDate = date.plus({minutes: addMin}).toUTC(WorldClockTimeZones.LIST[i].offsetMin);
+
+          let targetTime = tmpDate.toFormat('HHmm');
+
+          let dateStr = tmpDate.toFormat('yyyy-LL-dd (ccc) HH:mm:ss.SSS');
+
+          let element = document.getElementById(utc);
+          element.style.backgroundColor = findColor(targetTime);
+          element
+            .querySelectorAll('.clock')
+            .forEach((element) => {
+              element.innerHTML = dateStr;
+            });
+        }
+        addMin += 4;
+        setTimeout(counter, 1);
+      };
+      counter();
+    };
+    //testCounter();
+
   },
   methods: {
   },
@@ -55,6 +108,16 @@ export default {
 </script>
 
 <style>
+  /**
+   * noon    : #7ae0ff rgb(120, 220, 255) / decimal 8052991
+   * midnight: #0a191e rgb( 10,  25,  30) / decimal 661790
+   * diff    : 110, 195, 225 / deimal 7391201
+   *
+   * 86400sec. 1440min. 24hrs. / 43200sec. 720min. 12hrs.
+   *
+   * 7391201 / 43200 = 171.09261574
+   * 7391201 / 720 = 10265.5569444...
+   */
   .timezone .utc {
     float: left;
     /*font: 12px Monaco, Consolas, monospace;*/
